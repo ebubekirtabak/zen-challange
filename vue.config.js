7d7d3b6006509a075cb732d674b17a0ec6c411c4
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 23:37:01 +0300

    sass files has been import sass loader

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/scss/_variables.scss";
+          @import "@/scss/_mixins.scss";
+        `
+      }
+    }
+  }
+}
