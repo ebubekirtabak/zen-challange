5e688d4b0c8f8a49194cc6237db937eb0d019b7d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 19:02:26 +0300

    created: news card component.

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="news-card">
+        <div class="news-card__head">
+            <img class="news-card__head__image" src="https://4.bp.blogspot.com/-TBYj-oM-Stk/WyRR5b6JerI/AAAAAAAAojQ/yhKWgTVQFQQHuHbkS3MPmWde8yrItooygCLcBGAs/s1600/playerunknowns-battlegrounds-kasma-sorunu-troubleshooter01blogspot.jpg"/>
+            <div class="news-card__head__title">
+                League of Legends: Riot Point
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NewsCard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.news-card {
+    margin: 10px 0;
+    background-color: #f6f5f3;
+
+    &__head {
+        &__image {
+            width: 100%;
+            height: auto;
+        }
+
+        &__title {
+            margin: 10px 0;
+            font-size: 13px;
+            font-family: 'Avenir', Helvetica, Arial, sans-serif;
+            text-align: left;
+        }
+    }
+
+    &__footer {
+        
+    }
+}
+</style>
