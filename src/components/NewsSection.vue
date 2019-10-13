dc12a9abec7cca425f907214666ce0cad8bb7ad6
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 19:06:23 +0300

    News card import into news section.

--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="news-section">
-
+        <b-row>
+            <b-col cols="3" md="3">
+                <NewsCard />
+            </b-col>
+            <b-col cols="3" md="3">
+                <NewsCard />
+            </b-col>
+            <b-col cols="3" md="3">
+                <NewsCard />
+            </b-col>
+            <b-col cols="3" md="3">
+                <NewsCard />
+            </b-col>
+        </b-row>
+        
     </div>
 </template>
 
 <script>
+import NewsCard from '@/components/NewsCard.vue'
+
 export default {
   name: 'NewsSection',
+  components: {
+      NewsCard
+  },
   props: {
     msg: String
   }
