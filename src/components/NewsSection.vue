b2f31047e98f4e1c3e69e2871041115129087e32
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 14:56:08 +0300

    News Section component created.

---
+++ src/components/NewsSection.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="news-section">
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NewsSection',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.news-section {
+
+}
+</style>
