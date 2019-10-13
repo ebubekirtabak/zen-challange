d1627622e23b594dff34f2436777f15949a62487
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 19:08:03 +0300

    News section and whatsapp info import into home view.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="home">
-    <Carousel/>
+    <b-container>
+      <Carousel/>
+      <NewsSection/>
+      <WhatsAppInfo/>
+    </b-container>
+    {{ url }}
   </div>
 </template>
 
 <script>
 import Carousel from '@/components/Carousel.vue'
+import NewsSection from "@/components/NewsSection.vue"
+import WhatsAppInfo from '@/components/WhatsAppInfo.vue'
 
 export default {
   name: 'home',
   components: {
-    Carousel
+    Carousel,
+    NewsSection,
+    WhatsAppInfo
+  },
+  mounted() {
+  },
+  data() {
+    return {
+      url: process.env.NEWS_API_URL
+    }
   }
 }
 </script>
