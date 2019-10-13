5a26cfc27612918571cd48a2059b869bdfa64329
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 23:39:55 +0300

    Api request has been connect to ApiService.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,25 +13,35 @@
 import Carousel from '@/components/Carousel.vue'
 import NewsSection from "@/components/NewsSection.vue"
 import WhatsAppInfo from '@/components/WhatsAppInfo.vue'
+import ApiService from '@/services/api.service'
 
 export default {
   name: 'home',
   components: {
     Carousel,
     NewsSection,
-    WhatsAppInfo
+    WhatsAppInfo,
+    GridSection
   },
   mounted() {
-    axios
-    .get(this.url)
-    .then(response => (this.articles = response.data.articles))
+    ApiService.getAllNewsByJson(this.url)
+    .then(response => {
+      this.articles = response[0] && response[0].data.articles || []
+      this.moneynewsArticles = response[1] && response[1].data.articles || []
+      this.gridSectionArticles = response[2] && response[2].data.articles ||  []
+      this.highligthArticle = this.gridSectionArticles[0]
+      this.gridSectionArticles = this.gridSectionArticles.slice(1, this.gridSectionArticles.length)
+    })
     .catch(error => this.error = error)
   },
   data() {
     return {
       url: process.env.VUE_APP_NEWS_API_URL,
       error: '',
-      articles: []
+      articles: [],
+      moneynewsArticles: [],
+      gridSectionArticles: [],
+      highligthArticle: {}
     }
   }
 }
