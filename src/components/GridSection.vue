f83a4367da540c4c08ec9a9c682d65f79e86a24b
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 22:45:59 +0300

    GridSectionComponent Created.

---
+++ src/components/GridSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="news-section">
+    <b-row>
+      <b-col cols="12" md="6" >
+        <NewsCard v-if="highlightNews != null" v-bind:article="highlightNews" />
+      </b-col>
+      <b-col cols="12" md="6">
+        <b-row>
+          <b-col cols="12" md="6" v-for="article in articles" v-bind:key="article.id">
+            <NewsCard v-bind:article="article" />
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import NewsCard from '@/components/NewsCard.vue'
+
+export default {
+  name: 'GridSection',
+  data() {
+    return {
+      highlightNews: null
+    }
+  },
+  components: {
+    NewsCard
+  },
+  props: {
+    articles: Array
+  },
+  mounted() {
+    this.highlightNews = this.articles[0]
+    this.articles = this.articles.slice(1, this.articles.length)
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
