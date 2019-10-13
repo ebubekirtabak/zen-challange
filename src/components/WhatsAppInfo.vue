af1a0a5567804e710e437a16cca2f7ea272acc38
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-13 18:56:01 +0300

    Whatsapp info component added.

---
+++ src/components/WhatsAppInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-row align-v="center">
+        <b-col>
+            <div class="whatsapp-info">
+                <img class="whatsapp-info__logo" src="../assets/img/whatsapp.png" />
+                <span> +90 (216) 916 34 23</span>
+            </div>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+export default {
+  name: 'WhatsAppInfo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.whatsapp-info {
+    border-top: 1px solid #aba8a8;
+    border-bottom: 1px solid #aba8a8;
+    padding: 10px 0;
+    margin: 20px 0;
+    &__logo {
+        width: 32px;
+    }
+}
+</style>
